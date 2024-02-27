de7783db7773dfa9d8629e62fd41b8343500e80e
Mohd Ali <mohdali770409@gmail.com>
2024-02-27 17:41:12 +0530

    router created

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,23 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import About from "./pages/About";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+import Project from "./pages/Project";
+import Home from "./pages/Home";
+import Dashboard from "./pages/Dashboard";
 const App = () => {
-  return <div className="text-3xl text-red-200">App</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/about" element={<About />}></Route>
+        <Route path="/dashboard" element={<Dashboard />}></Route>
+        <Route path="/sign-in" element={<SignIn />}></Route>
+        <Route path="/sign-up" element={<SignUp />}></Route>
+        <Route path="/projects" element={<Project />}></Route>
+      </Routes>
+    </BrowserRouter>
+  );
 };
 
 export default App;
