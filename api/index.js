38f2effca749d7305f5ef00b93ab0963e25089f2
Mohd Ali <mohdali770409@gmail.com>
2024-02-29 23:35:40 +0530

    functionality added to signup page

--- api/index.js
+++ api/index.js
@@ -5,6 +5,7 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 import userRoutes from "./routes/user.route.js";
 import authRoutes from "./routes/auth.route.js";
+// import cors from "cors";
 dotenv.config();
 mongoose
   .connect(process.env.MONGO)
@@ -22,6 +23,7 @@ app.listen(3000, () => {
 
 app.use("/api/user", userRoutes);
 app.use("/api/auth", authRoutes);
+
 app.use((error, req, res, next) => {
   const statusCode = error.statusCode || 500;
   const message = error.message || "Internal server errror";
