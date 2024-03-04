8c5bafc17fdda9b24630ad91853c1da1eb4c494b
Mohd Ali <mohdali770409@gmail.com>
2024-03-04 22:12:06 +0530

    getposts api route created

--- api/controllers/post.controller.js
+++ api/controllers/post.controller.js
@@ -42,3 +42,62 @@ export const create = async (req, res, next) => {
     });
   }
 };
+
+export const getposts = async (req, res, next) => {
+  try {
+    // we will check from where we have to start and convert it into integer
+    const startIndex = parseInt(req.query.startIndex) || 0;
+    console.log(startIndex);
+    // set limit means how many post you want to show to user . if not given it will be 9
+    const limit = parseInt(req.query.limit) || 9;
+    // if order is asc then we will sort ascending wise else descending
+    console.log(limit);
+    const sortDirection = req.query.order === "asc" ? 1 : -1;
+    console.log(sortDirection);
+
+    const posts = await Post.find(
+      {...(req.query.userId && { userId: req.query.userId }),
+      ...(req.query.category && { category: req.query.category }),
+      ...(req.query.slug && { slug: req.query.slug }),
+      ...(req.query.postId && { _id: req.query.postId }),
+
+      ...(req.query.searchTerm && {
+        //regex is used to search if anything that matches to search. in this case if search matches to title and content
+        // or is used so that if search is present in title and content
+        //options here means capital letter and small letter be treated as same
+
+        $or: [
+          {
+            title: { $regex: req.query.searchTerm, $options: "i" },
+          },
+          { content: { $regex: req.query.searchTerm, $options: "i" } },
+        ],
+      })}
+    )
+      .sort({ updatedAt: sortDirection })
+      .skip(startIndex)
+      .limit(limit);
+    console.log("reached after db call");
+    const totalPosts = await Post.countDocuments();
+    console.log(totalPosts);
+    const now = new Date();
+    const oneMonthAgo = new Date(
+      now.getFullYear(),
+      now.getMonth() - 1,
+      now.getDate()
+    );
+    console.log(oneMonthAgo);
+    const lastMonthPost = await Post.countDocuments({
+      createdAt: { $gte: oneMonthAgo },
+    });
+
+    res.status(200).json({
+      posts,
+      totalPosts,
+      lastMonthPost,
+    });
+  } catch (error) {
+    console.log("unable to fetch data in post controller");
+    next(error);
+  }
+};
